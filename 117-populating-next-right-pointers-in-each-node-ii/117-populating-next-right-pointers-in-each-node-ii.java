83bcb38f05ce33e0e7d0826c178df1ac09f5ec58
Vaibhavgetch <61539841+Vaibhavgetch@users.noreply.github.com>
2022-02-19 17:20:41 +0530

    Time: 0 ms (100.00%), Space: 42 MB (50.64%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -22,24 +22,56 @@ class Node {
 */
 
 class Solution {
-    public Node connect(Node root) {
-        if(root == null) return root ;
-        Queue<Node> q = new ArrayDeque<>() ;
-        q.add(root);
+//     public Node connect(Node root) {
+//         if(root == null) return root ;
+//         Queue<Node> q = new ArrayDeque<>() ;
+//         q.add(root);
         
-        while( q.size() > 0){
-            int count = q.size() ;
-            for(int i = 0 ; i < count ; i++){
-                Node curr = q.remove() ;
+//         while( q.size() > 0){
+//             int count = q.size() ;
+//             for(int i = 0 ; i < count ; i++){
+//                 Node curr = q.remove() ;
                 
-                //Populating next pointer
-                if(i < count - 1){
-                    curr.next = q.peek() ;
+//                 //Populating next pointer
+//                 if(i < count - 1){
+//                     curr.next = q.peek() ;
+//                 }
+//                 if(curr.left != null) q.add(curr.left);
+//                 if(curr.right != null) q.add(curr.right);
+//             }
+//         }
+//         return root ;
+//     }
+    
+    // Approach 2: Without extra space
+      public Node connect(Node root) {
+        Node curr = root ;
+        while(curr != null){
+            Node head = null, tail = null ;
+            
+            while(curr != null ){
+                if(curr.left != null){
+                    if(head == null){
+                        head = tail = curr.left ;
+                    }
+                    else{
+                        tail.next = curr.left;
+                        tail = tail.next ;
+                    }
                 }
-                if(curr.left != null) q.add(curr.left);
-                if(curr.right != null) q.add(curr.right);
+                if(curr.right != null){
+                    if(head == null){
+                        head = tail = curr.right ;
+                    }
+                    else{
+                        tail.next = curr.right ;
+                        tail = tail.next ;
+                    }
+                }
+                curr = curr.next ;
             }
+            curr = head ;
         }
-        return root ;
+          return root ;
     }
 }
