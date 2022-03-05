78a6bb3b7fa702e940c71f654a14459f40baaade
Vaibhavgetch <61539841+Vaibhavgetch@users.noreply.github.com>
2022-03-05 21:39:46 +0530

    Time: 10 ms (55.30%), Space: 47.2 MB (32.67%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,7 +8,9 @@
  * }
  */
 class Solution {
-    
+    // We are returning the node whose left and right are not null
+    // That means at the LCA we will have some not null values from both sides
+    //In case if LCA is either p or q, it will return the first encounter
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
             
         if(root == null) return null ;
