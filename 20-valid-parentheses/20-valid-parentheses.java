0c7082405b2df1f00f9dc1e619aab26e0facad54
Vaibhavgetch <61539841+Vaibhavgetch@users.noreply.github.com>
2022-03-13 23:22:07 +0530

    Time: 2 ms (81.09%), Space: 41.8 MB (47.42%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> stk = new Stack<>() ;
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            
+            if(ch == ']'){
+                if(stk.isEmpty()) return false ;
+                if(stk.peek() == '['){
+                    stk.pop() ;
+                }
+                else return false ;
+            }
+            else if(ch == ')'){
+                if(stk.isEmpty()) return false ;
+                if(stk.peek() == '('){
+                    stk.pop() ;
+                }
+                else return false ;
+                
+            }
+            else if(ch == '}'){
+                if(stk.isEmpty()) return false ;
+                if(stk.peek() == '{'){
+                    stk.pop() ;
+                }
+                else return false ;
+                
+            }
+            else{
+                stk.push(ch) ;
+            }
+        }
+        if(stk.size() == 0) return true ;
+            return false ;
+    }
+}
