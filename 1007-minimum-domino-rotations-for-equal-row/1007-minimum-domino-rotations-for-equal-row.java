f33f4ec9bd637a6517e22fdffad0b697e93bef58
Vaibhavgetch <61539841+Vaibhavgetch@users.noreply.github.com>
2022-03-21 00:08:55 +0530

    Time: 9 ms (28.56%), Space: 88.7 MB (70.49%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -1,10 +1,17 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
         int length = tops.length + 1;
+        
+        //Array to maintain frequency of numbers in tops array
         int[] countA = new int[7];
+        
+        //Array to maintain frequency of numbers in bottoms array
         int[] countB = new int[7];
+        
+        //Array to maintain frequency of same numbers in tops and bottoms array
         int[] same = new int[7];
         
+        //Filling frequency of each no. in count arrays
         for(int i = 0 ; i < bottoms.length ; i++){
             countA[tops[i]]++;
             countB[bottoms[i]]++;
@@ -12,8 +19,10 @@ class Solution {
                 same[tops[i]]++ ;
             }
         }
-        
+        // Majority element in the arrays
         int majority = -1 ;
+        
+        //Checking for potential majority element
         for(int i = 1 ; i <=6 ; i++){
             
             if(countA[i] + countB[i] - same[i] == tops.length){
@@ -21,6 +30,8 @@ class Solution {
             }
         }
         
+        //Returning minimum of majority no. frequency 
+        //from both count arrays after removing no. of same occurences
         return majority == -1? -1 : Math.min(countA[majority],countB[majority]) - same[majority];
         
     }
