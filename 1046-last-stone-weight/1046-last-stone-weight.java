82108c5d03e266f7d87cca24663c1af42610cadb
Vaibhavgetch <61539841+Vaibhavgetch@users.noreply.github.com>
2022-04-07 23:36:39 +0530

    Time: 2 ms (76.58%), Space: 41.6 MB (47.75%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -2,12 +2,11 @@ class Solution {
     public int lastStoneWeight(int[] stones) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
+        //Adding array elements to Max-Heap
         for(int i : stones){
             pq.add(i);
         }
-        
-        
-        
+        //Removid top 2 max elements, and adding the difference back to the pq
         while(pq.size() != 1){
             pq.add(pq.remove() - pq.remove());
         }
